cb51d7ae8add173daa21a4bafca821ffd47992ba
Noe De Santo <desanto@ef5.ch>
2026-03-31 17:07:10 -0400

    Add slides

---
+++ .nix/flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Melange starter";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+    slides-lib.url = "git+https://git.ef5.ch/Ef5/slides-lib?dir=.nix";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, slides-lib }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            ocaml
+            dune_3
+            ocamlPackages.findlib
+            ocamlPackages.ocaml-lsp
+            ocamlPackages.melange
+            slides-lib.packages.${system}.slides
+            slides-lib.packages.${system}.server
+
+            deno
+          ];
+          buildInputs = with pkgs; [ ocamlPackages.melange ];
+        };
+      });
+}
---
+++ slides/main.ml
@@ -0,0 +1,37 @@
+open Domext
+open Slides.Lib
+
+type api = {
+  controls: controls;
+}
+
+external set_api: Dom.window -> api -> unit
+  = "slides" [@@mel.set]
+
+let setup_controls (ctr: controls): unit =
+  add_event_listener "keyup" (fun e_ ->
+    let e: Dom.keyboardEvent = Obj.magic e_ in
+    begin match get_key e with
+    | "ArrowLeft" -> ctr.backward ()
+    | "ArrowRight" -> ctr.forward ()
+    | _ -> ()
+    end
+  );
+  add_event_listener "hashchange" (fun _ ->
+    int_of_string_opt (get_hash ()) |> Option.value ~default:0 |> ctr.goto;
+  )
+
+let init () =
+  let data = Belt.Map.String.fromArray [|
+    "speaker", "No&eacute; De Santo"
+  |]
+  in
+
+  let root = get_element_by_id document "slides" in
+  let ctrl = init root data in
+  setup_controls ctrl;
+  set_api window { controls = ctrl };
+  print_endline ("Slideshow set in: " ^ (get_element_id root))
+
+let () =
+  init ()
